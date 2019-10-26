e68950decf906f5314d6c40ff522c31261792fe2
= <=>
2019-10-25 21:35:35 -0400

    Initialized active project

---
+++ flint_westwood_active/Assets/Scripts/FWStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FWStateMachine : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
