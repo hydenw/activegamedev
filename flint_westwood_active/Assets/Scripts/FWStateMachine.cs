bbdeabd04b6f56ef9872ee9c6561b22df1e1ba3f
= <=>
2019-10-25 21:41:12 -0400

    Added base state machine

--- flint_westwood_active/Assets/Scripts/FWStateMachine.cs
+++ flint_westwood_active/Assets/Scripts/FWStateMachine.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FWStateMachine : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
+    private Transform playerLocation;
+
+    protected virtual void InitializeState()
+    {
+        playerLocation = player.transform;
+    }
+    protected virtual void StateUpdate() {}
+    
+    protected virtual void StateFixedUpdate() {}
     void Start()
     {
         
@@ -11,6 +22,11 @@ public class FWStateMachine : MonoBehaviour
 
     void Update()
     {
-        
+        StateUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        StateFixedUpdate();
     }
 }
