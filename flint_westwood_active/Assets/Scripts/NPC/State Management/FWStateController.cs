77da350a671e9db8b08549311da74f9d63889f23
hw-2030 <hydenw2030@gmail.com>
2019-11-08 20:22:48 -0500

    Added files for beta build

---
+++ flint_westwood_active/Assets/Scripts/NPC/State Management/FWStateController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FWStateController : MonoBehaviour
+{
+    public GameObject player;
+    public Weapon npcWeapon;
+    public Waypoint[] waypoints;
+
+    private FWStateManager _manager;
+    
+
+    private void InitializeStateManager()
+    {
+//        PatrolWaypointState waypointState = new PatrolWaypointState(waypoints);
+//        waypointState.AddStateTransition(NPCStateTransition.SawPlayer, NPCState.Track);
+        TargetPlayerState targetPlayerState = new TargetPlayerState(30f, 5f, npcWeapon);
+
+        _manager = new FWStateManager();
+//        _manager.AddNewState(waypointState);
+        _manager.AddNewState(targetPlayerState);
+    }
+    
+    public void SwitchState(NPCStateTransition transition)
+    {
+        _manager.HandleTransition(transition);
+    }
+    void Start()
+    {
+        InitializeStateManager();   
+    }
+
+    void Update()
+    {
+        if (player && this.gameObject)
+        {
+            _manager.CurrentState.ShouldStateChange(player, this.gameObject);
+            _manager.CurrentState.ExecuteCurrentStateBehavior(player, this.gameObject);
+        }
+    }
+}
